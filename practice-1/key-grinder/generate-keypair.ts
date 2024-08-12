67e14fc4f038f79df8804692702d9412a6e2c26a
zekainhuman1 <zekainhuman@gmial.com>
2024-08-12 22:47:07 +0300

    HomeWork2

---
+++ practice-1/key-grinder/generate-keypair.ts
@@ -0,0 +1,15 @@
+import { Keypair } from "@solana/web3.js";
+
+const prefix = "pok".toLowerCase();
+
+let keypair: Keypair;
+let publicKey: string;
+
+do {
+  keypair = Keypair.generate();
+  publicKey = keypair.publicKey.toBase58().toLowerCase();
+} while (!publicKey.startsWith(prefix));
+
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+console.log(`The secret key is: `, keypair.secretKey);
+console.log(`✅ Finished!`);
---
+++ practice-2/load-keypair.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { Keypair } from "@solana/web3.js";
+
+let privateKey = process.env["SECRET_KEY"];
+if (privateKey === undefined) {
+  console.log("Add SECRET_KEY to .env!");
+  process.exit(1);
+}
+const asArray = Uint8Array.from(JSON.parse(privateKey));
+const keypair = Keypair.fromSecretKey(asArray);
+
+console.log(`Public key: ${keypair.publicKey.toBase58()}`);
